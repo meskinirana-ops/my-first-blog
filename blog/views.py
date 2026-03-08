6967adb72ab59a440d0e922b95f5792b2cc9e24c
Rana Meskini <meskinirana@gmail.com>
2026-03-08 16:44:13 +0100

    My Django Girls app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post  # si tu as un modèle Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()  # récupère tous les posts
+    return render(request, 'blog/post_list.html', {'posts': posts})
